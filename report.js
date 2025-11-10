6e3f4b044603e9fc2619dd3665a94d824afa0492
unknown <asmaanika377@gmail.com>
2025-11-10 21:09:22 +0600

    Removed sensitive keys

--- report.js
+++ report.js
@@ -2,7 +2,7 @@ const newman = require('newman');
 noderequire('dotenv').config();
  
 newman.run({
-    collection:`https://api.postman.com/collections/45419119-fabfb4f2-6f8a-49ea-9e37-dd084216d627?access_key=${process.env.pmatKey}`,
+    collection:`https://api.postman.com/collections/45419119-fabfb4f2-6f8a-49ea-9e37-dd084216d627?access_key= ${process.env.pmatKey}`,
 
     //collection:require('./collection/dmoney-user-api-collection.json'),
     reporters: 'htmlextra',
